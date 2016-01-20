de99a669ea5ace571bb8763f9a9703086b30b1d0
Victor Tang <victortang1992@gmail.com>
2016-01-20 12:16:26 -0800

    pull from last project and convert

---
+++ public/app.js
@@ -0,0 +1,9 @@
+(function() {
+    //start of function
+  var app = angular.module('filescanner', []);
+
+app.controller('MainCtrl', ['$scope', '$http', '$window', function($scope, $http, $window){
+
+}]);//end of controller
+  //end of function
+})();
---
+++ routes/routes.js
@@ -0,0 +1,34 @@
+var Q = require('q');
+
+// routes.js
+module.exports = function(app) {
+	
+function searchBing(query, page, res) {
+    var request=require('request');
+    var url= !!(page) ? "https://api.datamarket.azure.com/Bing/Search/v1/Image?Query='"+query+"'&$format=JSON&$skip="+(page*10) : "https://api.datamarket.azure.com/Bing/Search/v1/Image?Query='"+query+"'&$format=JSON&$skip="+0 ;
+    var key="gpfMNHL5VVhAnl0FaDb90ykr8rMRNYUgYs6f/uK9xiU";
+	var deferred = Q.defer();
+    request.get(url, {auth: { user: key, password: key} }, function (error, result) {
+		deferred.resolve(result.body);
+    })
+	deferred.promise.then(function (value) {
+		value = JSON.parse(value);
+		var arr = value.d.results;
+		res.send(arr.slice(0,10));
+	});
+}
+function storeSearch(item){
+	if(!app.lastSearch){
+		//init app.lastSearch
+		app.lastSearch = [];
+	}
+	app.lastSearch.push(item)
+}
+    // =====================================
+    // HOME PAGE (with login links) ========
+    // =====================================
+    app.get('/', function(req, res) {
+        res.render('index.ejs'); // load the index.ejs file
+    });
+
+}
