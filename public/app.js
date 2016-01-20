8c1510affe5de73620f46a1f284a724444623221
Victor Tang <victortang1992@gmail.com>
2016-01-20 12:34:56 -0800

    upload button and function

--- public/app.js
+++ public/app.js
@@ -4,6 +4,37 @@
 
 app.controller('MainCtrl', ['$scope', '$http', '$window', function($scope, $http, $window){
 
+	$scope.scanFile = function(){
+	    var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
+        if (regex.test($("#fileUpload").val().toLowerCase())) {
+            if (typeof (FileReader) != "undefined") {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    var table = $("<table />");
+                    var rows = e.target.result.split("\n");
+                    for (var i = 0; i < rows.length; i++) {
+                        var row = $("<tr />");
+                        var cells = rows[i].split(",");
+                        for (var j = 0; j < cells.length; j++) {
+                            var cell = $("<td />");
+                            cell.html(cells[j]);
+                            row.append(cell);
+                        }
+                        table.append(row);
+                    }
+                    $("#editor").html('');
+                    $("#editor").append(table);
+                }
+                reader.readAsText($("#fileUpload")[0].files[0]);
+            } else {
+                alert("This browser does not support HTML5.");
+            }
+        } else {
+            alert("Please upload a valid CSV file.");
+        }
+	}
+	
+	
 }]);//end of controller
   //end of function
 })();
