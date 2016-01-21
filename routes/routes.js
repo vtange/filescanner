a52d27129eeea8f9ff2e76a0ac9d4d97e95ef2eb
Victor Tang <victortang1992@gmail.com>
2016-01-21 00:54:47 -0800

    file upload possible

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,19 @@
 var Q = require('q');
+var multer  = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage, dest: './tmp/'});
 
+var uploadFile = function(req, res) {
+	console.log(req.file);
+}
 // routes.js
 module.exports = function(app) {
+
     // =====================================
     // HOME PAGE (with login links) ========
     // =====================================
     app.get('/', function(req, res) {
         res.render('index.ejs'); // load the index.ejs file
     });
-	app.post('/', function(req, res){
-		console.log("got something")
-		
-	});
+	app.post('/',upload.single('userfile'), uploadFile);	//.single('userfile') must match name of file in app.js -> formData.append("userfile",
 }
