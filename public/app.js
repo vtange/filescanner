0b7e4906b0f81b3e22ca7ebaa869adb91b28c864
Victor Tang <victortang1992@gmail.com>
2016-01-20 17:47:12 -0800

    remove extra

--- public/app.js
+++ public/app.js
@@ -11,15 +11,16 @@ app.controller('MainCtrl', ['$scope', '$http', '$window', function($scope, $http
             if (typeof (FileReader) != "undefined") {
                 var reader = new FileReader();
                 reader.onload = function (e) {
-					var table = $("<table></table>");
-					var rows = e.target.result.split("\n");
-					for (var i = 0; i < rows.length; i++) {
-						var row = $("<tr />");
-						row.html(rows[i]);
-						table.append(row);
-					}
-                    $("#editor").html('');
-                    $("#editor").append(table);
+					console.log(e.target.result);
+//					var table = $("<table></table>");
+//					var rows = e.target.result.split("\n");
+//					for (var i = 0; i < rows.length; i++) {
+//						var row = $("<tr />");
+//						row.html(rows[i]);
+//						table.append(row);
+//					}
+//                    $("#editor").html('');
+//                    $("#editor").append(table);
                 }
                 reader.readAsText($("#fileUpload")[0].files[0]);
             } else {
